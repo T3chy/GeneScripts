85a10f0cdc1dba246540b8b946fdd2b880541f8a
Elam Day <elam.day.friedland@gmail.com>
2020-06-02 20:41:41 -0700

    added proof of concept R stuff: todo get user input on what to graph and what to name files

--- qc.bat
+++ qc.bat
@@ -5,6 +5,7 @@ dir=$1
 echo "$dir"
 echo "$plinkPath"
 echo "cleaning data..."
-$plinkPath --allow-no-sex --make-bed --geno .02 --mind .02 --noweb --vcf "$dir"
+$plinkPath --allow-no-sex --make-bed --geno .02 --mind .02 --vcf "$dir" --out "$dir"
 echo "generating Hardy-Weinberg figures..."
-$plinkPath --allow-no-sex --noweb --hardy --vcf "$dir"
+$plinkPath --allow-no-sex --hardy --vcf "$dir" --out "$dir"
+Rscript -e "hwe <- read.table("$dir" + ".hwe"); head(hwe)
