e69f092fd1e0429ea6e9fe782fa88ce07c6571b7
t3chy <elam.day.friedland@gmail.com>
2020-06-14 11:14:32 -0700

    added more script documentation TODO run it all the way through from a clean env to test

--- qc.bat
+++ qc.bat
@@ -1,40 +1,48 @@
 #!/bin/bash
+### EXAMPLE USAGE- if your plnk exexutable is in the downloads folder and you are in the directory with this script in it, you would run this script like so:  ###
+### ./qc.bat /path/to/data/data.vcf ~/Downloads/ ###
 # QC time
+#inputs to this script are the path to a vcf of your data and a path to the directory you plink app is in
 plinkPath=$2./plink
 dir=$1
-cd /mnt/HDD/Data/
-#inputs to this script are the path to a vcf of your data and a path to the directory you plink app is in
+cd /mnt/HDD/Data/ #this is specific to my computer- TODO generalize / abstract this
+# confirmation of directory inputs and tells user inital qc is starting
 echo "$dir"
 echo "$plinkPath"
-# confirmation of directory inputs and tells user inital qc is starting
 echo RECODE
+# change vcf to binary .bed for use w/ plink
 $plinkPath --allow-no-sex --make-bed --recode --vcf "$dir"
 echo GENO
-$plinkPath --allow-no-sex --make-bed --geno .04 --file "plink" --out "data_genoed"
 # call geno first to take out SNPs with bad overall call rate
-# prolly make a grep call to log file to print out num of inidivduals removed or smth 
+$plinkPath --allow-no-sex --make-bed --geno .04 --file "plink" --out "data_genoed"
 echo MIND
+# call mind after geno to take out individuals with bad overall call rate considering SNPs with bad call rates
 $plinkPath --allow-no-sex --make-bed  --mind .05 --bfile "data_genoed" --out "data_minded"
-# call geno after mind to take out individuals with bad overall call rate
 # prolly make another grep call here to do the same thing but for mind
-echo "generating Hardy-Weinberg figures..."
 echo HARDY
+#generate hwe equalibrium data
 $plinkPath --allow-no-sex --hardy --bfile "data_genoed"
+# filter hwe file to remove all individuals w/ p values < 1*10^-5
 awk '{ if ($9 < 0.00001) print $0 }' plink.hwe > plinkfilteredhwe.hwe
 echo HARDY1
+# this filters the actual binary file to individuals w p values > 1*10^-6
 $plinkPath --allow-no-sex --bfile "data_genoed" --hwe 1e-6 --make-bed --out "hwe_filter_step1"
 echo HARDY2
+# for case-control data, this filters the subjects too to a looser standard
 $plinkPath --allow-no-sex --bfile "hwe_filter_step1" --hwe 1e-10 "include-nonctrl" --make-bed --out "data_hwed"
 echo PIHAT
+# pihat IBD estimate to .genome file with a min pihat value of .2
 $plinkPath --allow-no-sex --bfile "data_hwed" --genome --min 0.2 --out "pihat_min0.2"
+#filters .genome file to only over .9 pihat- this is just for graphing and stuff
 awk '{if ($8 > 0.9) print $0 }' pihat_min0.2.genome > done_pihat.genome
 echo FILTERFOUNDERS
+#filters founders
 $plinkPath --allow-no-sex --bfile "data_hwed" --filter-founders --make-bed --out "data_founderfiltered"
 echo FINALPIHAT
+#pihat on the actual data after founder filtered, filtering for pihat below .2
 $plinkPath --allow-no-sex --bfile "data_founderfiltered" --make-bed --genome --min 0.2 --out "pihat_min0.2_in_founders"
+#generate allele frequency for analysis / visualization
 $plinkPath --allow-no-sex --bfile "pihat_min0.2_in_founders" --freq --out "freq"
-#generate HWE figures TODO take out outlier SNPs found using this analysis (p value <8*10^-5)
-#use --recode to make map and ped files for --freq and stuff
 
 
 
