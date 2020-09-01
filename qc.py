ba5e0f63601d16093c02e536b2d3dd9082565e90
Elam Day <elam.day.friedland@gmail.com>
2020-09-01 16:27:34 -0700

    general stuff

--- qc.py
+++ qc.py
@@ -1,4 +1,5 @@
 import subprocess #TODO figure out how to pipe output and also actually run plink, make funcs return plink output and grep it 
+# take out redundant genome 
 import pandas as pd
 import matplotlib.pyplot as plt
 import os
@@ -16,6 +17,11 @@ def parser(stdoutput):
         return 0
     stdoutput = str(stdoutput.stdout)
     print("graph generated")
+def king(file, out="king"):
+    king = subprocess.Popen(args=["king", "-b", file + ".bed", "--related"],stdout=subprocess.PIPE,encoding="utf-8")
+    output = king.stdout
+    getterms(output,[""])
+    return(out)
 def recode(file, out="plink"):
     recode = subprocess.run(args=["plink","--allow-no-sex", "--make-bed", "--recode", "--file", file, "--out", out],stdout=subprocess.PIPE, encoding='utf-8') ### echo "recoding to binary..." 
     output = recode.stdout
@@ -83,29 +89,27 @@ def main(steps,startfile,outdir): # add option to change around thresholds
             out = mind(out)
         elif step == "4":
             hardy(out, Filter=True)
-        elif step ==  "5":
-           out = genome(out,filter=True)
-        elif step == "6":
+        elif step == "5":
             out = filterfounders(out)
-        elif step == "7":
+        elif step == "6":
             hardy(out)
-        elif step == "8":
+        elif step == "7":
             genome(out)
             # todo workin on this to make it actually useful
-        elif step == "9":
+        elif step == "8":
             out = freq(out)
             # todo workin on this to make it actually useful
-        elif step == "10":
+        elif step == "9":
            print("generating frequency graph...")
            freqgraph = subprocess.run(args=["Rscript", "-e", "require(\"tidyverse\"); setwd(getwd()); frq <- read.table(\"freq.frq\", skip=1); pdf(\"freq.pdf\"); hist(frq$V5);dev.off()"],stdout=subprocess.PIPE, stderr=subprocess.PIPE,encoding='utf-8')
            parser(freqgraph)
            print("graph saved to \'freq.pdf\'")
-        elif step == "11":
+        elif step == "10":
             print("generating hwe graph...")
             hwegraph = subprocess.run(args=["Rscript", "-e", "require(\"tidyverse\"); setwd(getwd()); hwe <- read.table(\"plink.hwe\",skip=1); pdf(\"hwe.pdf\"); hist(hwe$V9, main=\"Hardy-Weinberg Test Density\", xlab=\"p-value\");dev.off()"],stdout=subprocess.PIPE, stderr=subprocess.PIPE,encoding='utf-8')
             parser(hwegraph)
             print("graph saved to \'hwe.pdf\'")
-        elif step == "12":
+        elif step == "11":
             print("generating pi-hat value distribution...")
             pihatgraph = subprocess.run(args=["Rscript", "-e", "require(\"tidyverse\"); setwd(getwd()); ibd <- read.table(\"IBD.genome\",skip=1); pdf(\"IBD.pdf\");hist(ibd$V10, main=\"Pi-Hat Values\",xlab=\"pi-hat\");dev.off()"],stdout=subprocess.PIPE,stderr=subprocess.PIPE,encoding='utf-8')
             parser(pihatgraph)
@@ -119,27 +123,27 @@ outputdir = input("""path where you'd like your results?
 example: /home/elamd/projects/GeneScripts/ will put all of the intermediary files, summary stats, and pdfs in the folder "GeneScripts"
 """)
  #outputdir = '/home/elamd/projects/GeneScripts/'
+# ask laura about adding KING IBD and stuff
 steps = input ("""
 planned qc steps:
 1) recode vcf to binary (skip this if you already have .bim and .map files)
 2) --geno .05 to remove all markers (SNPs) with more than 5% missingness
 3) --mind .05 to remove all individuals with more than 5% missingness
 4) --hwe .05 to filter out all individuals with a p value lower than 1e-6
-5) --genome --min .05 to filter for IBD
-8) --filter-founders to filter out all samples with at least one known parental ID
+5) --filter-founders to filter out all samples with at least one known parental ID
 
 planned summary data generation steps:
 6) --hardy to generate HWE data for p-value visualization
 7) --genome to generate IBD data report for visualization
-9) --freq to generate MAF data report for visualization
+8) --freq to generate MAF data report for visualization
 
 planned graphing steps:
-10) graph p-values for HWE
-11) graph MAF data
-12) graph distribution of pi-hat values for IBD pairs
+9) graph p-values for HWE
+10) graph MAF data
+11) graph distribution of pi-hat values for IBD pairs
 please enter any number of these steps in the order you'd by number seperated with spaces, or press enter to run them all in the order presented!
 """)
 if __name__ == "__main__":
     if steps == "":
-        steps = "1 2 3 4 5 6 7 8 9 10 11 12"
+        steps = "1 2 3 4 5 6 7 8 9 10 11"
     main(steps,inputfile,outputdir)
